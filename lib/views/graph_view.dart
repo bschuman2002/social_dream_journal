c8aaff3c2b4c50962b3e0bd7551bc99d7e8e6312
Ben Schuman <bschuman2002@gmail.com>
2023-11-30 12:51:22 -0500

    link graph points to respective journal entries

--- lib/views/graph_view.dart
+++ lib/views/graph_view.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:social_dream_journal/views/journal_entry_view.dart";
 import "package:social_dream_journal/widgets/navbar.dart";
 import "package:fl_chart/fl_chart.dart";
 import "package:social_dream_journal/viewmodels/journal_entry_list_view_model.dart";
@@ -12,9 +13,16 @@ class GraphView extends StatelessWidget {
   Widget build(BuildContext context) {
     List<JournalEntryViewModel> userEntries =
         Provider.of<JournalListProvider>(context).userJournalViewModels.toList()
-          ..sort((a, b) => b.date.compareTo(a.date));
+          ..sort((a, b) => a.date.compareTo(b.date));
     return Scaffold(
-      body: TrendLineChart(userEntries: userEntries),
+      body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        const Padding(
+            padding: EdgeInsets.only(top: 100, left: 16),
+            child: Text('Sleep Score Trends',
+                style: TextStyle(color: Colors.black, fontSize: 30))),
+        const SizedBox(height: 100),
+        SizedBox(height: 500, child: TrendLineChart(userEntries: userEntries)),
+      ]),
       bottomNavigationBar: const NavBar(
         pageIndex: 2,
       ),
@@ -31,66 +39,86 @@ class TrendLineChart extends StatelessWidget {
     final List<Point> data = userEntries
         .map((entry) => Point(entry.date, entry.sleepScore.toDouble()))
         .toList();
+
     return Padding(
-      padding: EdgeInsets.all(16.0),
-      child: LineChart(
-        LineChartData(
-          gridData: FlGridData(show: false),
-          titlesData: FlTitlesData(
-            topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
-            rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
-            leftTitles: AxisTitles(
-                sideTitles: SideTitles(
-              showTitles: true,
-              reservedSize: 25,
-              interval: 1,
-            )),
-            bottomTitles: AxisTitles(
-                sideTitles: SideTitles(
-              showTitles: true,
-              reservedSize: 22,
-              interval: 86400000,
-              // You can customize the format and appearance further if needed
-              getTitlesWidget: (value, meta) {
-                final date = DateTime.fromMillisecondsSinceEpoch(value.toInt());
-                // if (value == meta.max) {
-                //   return Container();
-                // }
-                return Text(
-                  '${date.month}/${date.day}',
-                  style: TextStyle(
-                    color: Color(0xff68737d),
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
-                  ),
-                );
-              },
-            )),
-          ),
-          borderData: FlBorderData(
-            show: true,
-            border: Border.all(color: const Color(0xff37434d), width: 1),
-          ),
-          minX: data.first.x.millisecondsSinceEpoch.toDouble(),
-          maxX: data.last.x.millisecondsSinceEpoch.toDouble(),
-          minY: 0,
-          maxY: 10,
-          lineBarsData: [
-            LineChartBarData(
-              spots: data
-                  .map((point) => FlSpot(
-                        point.x.millisecondsSinceEpoch.toDouble(),
-                        point.y.toDouble(),
-                      ))
-                  .toList(),
-              isCurved: true,
-              dotData: FlDotData(show: true),
-              belowBarData: BarAreaData(show: false),
+        padding: const EdgeInsets.only(top: 0, right: 30, bottom: 16, left: 16),
+        child: LineChart(
+          LineChartData(
+            gridData: FlGridData(show: false),
+            titlesData: FlTitlesData(
+              topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+              rightTitles:
+                  AxisTitles(sideTitles: SideTitles(showTitles: false)),
+              leftTitles: AxisTitles(
+                  sideTitles: SideTitles(
+                showTitles: true,
+                reservedSize: 25,
+                interval: 1,
+              )),
+              bottomTitles: AxisTitles(
+                  sideTitles: SideTitles(
+                showTitles: true,
+                reservedSize: 40,
+                interval: (data.last.x.millisecondsSinceEpoch.toDouble() -
+                        data.first.x.millisecondsSinceEpoch.toDouble()) /
+                    4,
+                // You can customize the format and appearance further if needed
+                getTitlesWidget: (value, meta) {
+                  final date =
+                      DateTime.fromMillisecondsSinceEpoch(value.toInt());
+                  if (value == meta.min) {
+                    return Container();
+                  }
+                  return Padding(
+                      padding: EdgeInsets.only(top: 10),
+                      child: Text(
+                        '${date.month}/${date.day}',
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 92, 103, 222),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ));
+                },
+              )),
             ),
-          ],
-        ),
-      ),
-    );
+            borderData: FlBorderData(
+              show: true,
+              border: Border.all(color: const Color(0xff37434d), width: 1),
+            ),
+            minX: data.first.x.millisecondsSinceEpoch.toDouble(),
+            maxX: data.last.x.millisecondsSinceEpoch.toDouble(),
+            minY: 0,
+            maxY: 10,
+            lineBarsData: [
+              LineChartBarData(
+                spots: data
+                    .map((point) => FlSpot(
+                          point.x.millisecondsSinceEpoch.toDouble(),
+                          point.y.toDouble(),
+                        ))
+                    .toList(),
+                isCurved: false,
+                dotData: const FlDotData(show: true),
+                belowBarData: BarAreaData(show: false),
+              ),
+            ],
+            lineTouchData: LineTouchData(
+              touchCallback: (event, touchResponse) {
+                if (event is FlTapUpEvent) {
+                  if (touchResponse?.lineBarSpots != null) {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => journal_entry_view(
+                                entry: userEntries[touchResponse!
+                                    .lineBarSpots!.first.spotIndex])));
+                  }
+                }
+              },
+            ),
+          ),
+        ));
   }
 }
 
